7c2551391265cb829e08509e9ab7f0a9d637f7e9
Akus0ni <asoni7667@gmail.com>
2017-09-10 11:45:11 +0530

    Python 2.7 first

---
+++ exe5.py
@@ -0,0 +1,17 @@
+my_name = 'Aku Soni'
+my_age = 24 # no lying
+my_height = 74 # inches
+my_weight = 75 #kg
+my_eyes = 'Brown'
+my_teeth = 'Yellowish White'
+my_hair = 'Brown'
+
+print "Lets talk about %s." %my_name
+print "He's %d inches tall." %my_height
+print "He's %d Kg heavy." %my_weight
+print "Actually thats not too heavy."
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "his teeth are usually %s depending on the coffee." %my_teeth
+
+print "if I add %d, %d and %d I get %d." %(
+	my_age, my_height, my_weight, my_age+my_height+my_weight)
